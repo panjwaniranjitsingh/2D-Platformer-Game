f26434120b79ab47ca51c876c7f7caa9cf054ce6
Panjwani Ranjit Singh <80108326+panjwaniranjitsingh@users.noreply.github.com>
2021-03-26 20:59:21 +0530

    Revert "Chapter 4 Assignment to my repository"
    This reverts commit 6186844a0711c0f5dc166e33f8fb35b9bc47a5c6.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    private BoxCollider2D playerCollider;
+    public BoxCollider2D playerCollider;
     public float initialYoffset, initialYsize, crouchYoffset = 0.07785285f, crouchYsize= 1.278029f;
     public float speed;
-    private Rigidbody2D rigbod2d;
-    public float jump;
     // Start is called before the first frame update
     void Start()
     {
-        playerCollider = gameObject.GetComponent<BoxCollider2D>();
+        playerCollider = GetComponent<BoxCollider2D>();
         initialYoffset=playerCollider.offset.y; 
         initialYsize = playerCollider.size.y;
-        rigbod2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -25,28 +22,19 @@ public class PlayerController : MonoBehaviour
     {
 
         float horizontal = Input.GetAxisRaw("Horizontal");
-        //Debug.Log("Player Speed is:"+horizontal);
-        float vertical = Input.GetAxisRaw("Vertical");
-        //Debug.Log("Player can jump : "+vertical);
-        PlayerMovementAnimation(horizontal,vertical);
-        MovePlayer(horizontal,vertical);
+        //Debug.Log("Player Speed is:"+playerSpeed);
+        PlayerMovementAnimation(horizontal);
+        MovePlayer(horizontal);
     }
 
-    private void MovePlayer(float horizontal,float vertical)
+    private void MovePlayer(float horizontal)
     {
-        //Move player horizontally
         Vector3 position = transform.position;
         position.x += horizontal * speed * Time.deltaTime;
         transform.position = position;
-
-        //Move player vertically
-        if(vertical>0)
-        {
-            rigbod2d.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
-        }
     }
 
-    private void PlayerMovementAnimation(float horizontal,float vetical)
+    private void PlayerMovementAnimation(float horizontal)
     {
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
@@ -73,8 +61,8 @@ public class PlayerController : MonoBehaviour
             playerCollider.offset = new Vector2(playerCollider.offset.x, initialYoffset);
             playerCollider.size = new Vector2(playerCollider.size.x, initialYsize);
         }
-        
-        if (vetical > 0)
+        float canJump = Input.GetAxisRaw("Vertical");
+        if (canJump > 0)
         {
             animator.SetBool("Jump", true);
         }
