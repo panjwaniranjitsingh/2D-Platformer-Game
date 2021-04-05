f5f27b8967a52cdee07ffa381f9c439bb184ba30
Panjwani Ranjit Singh <80108326+panjwaniranjitsingh@users.noreply.github.com>
2021-04-05 17:54:15 +0530

    Feature 6 - Add enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            //Player Dies so Restart Level
+            Debug.Log("Player Die due to enemy contact");
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            playerController.PlayerDie();
+        }
+    }
+}
